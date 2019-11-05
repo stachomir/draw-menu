13b8498c32d9e06eec6472b52138f0aa80d11a2f
stachomir <stanislaw.wadowski@performgroup.com>
2019-11-05 15:18:25 +0100

    initial commit

---
+++ src/test/java/com/stan/menu/MenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stan.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
